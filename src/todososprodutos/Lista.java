90b27d33a37c434472b87345c2fee1eda451ecf0
Lucas de Lucas <delucas.lucas@gmail.com>
2020-11-14 13:45:05 -0300

    atualizado

--- src/todososprodutos/Lista.java
+++ src/todososprodutos/Lista.java
@@ -37,45 +37,41 @@ public class Lista {
     }
 
     public void adicionaProdutoNaLista() {
-        if (listaDeProdutos.size() > 0) {
-            String codigo = inString("Digite o código do produto: ");
-            for (int i = 0; i < listaDeProdutos.size(); i++) {
-                if (verificaExistenciaMesmoCodigo(codigo) == false) {
-                    int opcao = inInt(
-                            "\n=========| Digite a opção de cadastro |=========\n[1] Nome, Código, Quantidade, Aviso estoque baixo\n[2] Nome, Código, Quantidade\n[3] Nome, Código\n[4] Cancelar cadastro\n================================================");
-                    switch (opcao) {
-                        case 1:
-                            String nome = inString("Digite o nome do produto: ");
-                            int quantidade1 = inInt("Digite a quantia em estoque: ");
-                            int avisoEstoqueBaixo = inInt("Digite a quantia para aviso de estoque baixo: ");
-                            listaDeProdutos.add(new Produto(nome, codigo, quantidade1, avisoEstoqueBaixo));
-                            break;
+        String codigo = inString("Digite o código do produto: ");
+        for (int i = 0; i < listaDeProdutos.size(); i++) {
+            if (verificaExistenciaMesmoCodigo(codigo) == false) {
+                int opcao = inInt(
+                        "\n=========| Digite a opção de cadastro |=========\n[1] Nome, Código, Quantidade, Aviso estoque baixo\n[2] Nome, Código, Quantidade\n[3] Nome, Código\n[4] Cancelar cadastro\n================================================");
+                switch (opcao) {
+                    case 1:
+                        String nome = inString("Digite o nome do produto: ");
+                        int quantidade1 = inInt("Digite a quantia em estoque: ");
+                        int avisoEstoqueBaixo = inInt("Digite a quantia para aviso de estoque baixo: ");
+                        listaDeProdutos.add(new Produto(nome, codigo, quantidade1, avisoEstoqueBaixo));
+                        break;
 
-                        case 2:
-                            String nome1 = inString("Digite o nome do produto: ");
-                            int quantidade2 = inInt("Digite a quantia em estoque: ");
-                            listaDeProdutos.add(new Produto(nome1, codigo, quantidade2));
-                            break;
+                    case 2:
+                        String nome1 = inString("Digite o nome do produto: ");
+                        int quantidade2 = inInt("Digite a quantia em estoque: ");
+                        listaDeProdutos.add(new Produto(nome1, codigo, quantidade2));
+                        break;
 
-                        case 3:
-                            String nome2 = inString("Digite o nome do produto: ");
-                            listaDeProdutos.add(new Produto(nome2, codigo));
-                            break;
+                    case 3:
+                        String nome2 = inString("Digite o nome do produto: ");
+                        listaDeProdutos.add(new Produto(nome2, codigo));
+                        break;
 
-                        case 4:
-                            System.out.println("Cadastro negado pelo usuário!");
-                            break;
+                    case 4:
+                        System.out.println("Cadastro negado pelo usuário!");
+                        break;
 
-                        default:
-                            break;
-                    }
-
-                } else {
-                    System.out.println("Produto com o código: " + codigo + " não encontrado!");
+                    default:
+                        break;
                 }
+
+            } else {
+                System.out.println("Produto com o código: " + codigo + " já existe!");
             }
-        } else {
-            System.out.println("Não há produtos cadastrados!");
         }
     }
 
